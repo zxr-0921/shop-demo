d9ebac5640a22820a490e564de57f84eec1052c1
zxr <zxr@qq.com>
2023-01-18 10:33:21 +0800

    second commit

--- src/main/java/com/zxr/hh/git.java
+++ src/main/java/com/zxr/hh/git.java
@@ -8,5 +8,9 @@ public class git {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
