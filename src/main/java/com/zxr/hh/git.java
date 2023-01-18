2d2122b9886ccfc37277462f7929c3f0c8f95eba
zxr <zxr@qq.com>
2023-01-18 10:13:16 +0800

    first commit

---
+++ src/main/java/com/zxr/hh/git.java
@@ -0,0 +1,12 @@
+package com.zxr.hh;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
